fca240c6d7a02a74117b7a2c14fb7cb98c7a2bc9
Sviatik <s.stetsyak@gmail.com>
2017-03-06 19:06:14 +0200

    Add files via upload

---
+++ hw6.py
@@ -0,0 +1,19 @@
+import sys
+
+import dockermap.api
+
+docker_url = 'unix://var/run/docker.sock'
+tag_name = sys.argv[1]
+
+
+docker_conn = dockermap.api.DockerClientWrapper(docker_url)
+docker_file = dockermap.api.DockerFile('centos7/hw:latest')
+
+docker_file.run_all('yum install -y httpd')
+docker_file.run_all('echo Homework6! > /var/www/html/index.html')
+
+docker_file.entrypoint = ['/usr/sbin/httpd']
+docker_file.command = ['-D', 'FOREGROUND']
+
+
+docker_conn.build_from_file(docker_file, tag_name)
